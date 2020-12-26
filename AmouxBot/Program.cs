681e63d5ba2bcbf536f3da1c54c2c6202ac74a1c
Rafael Vissontai de Oliveira <rafael.vissontai@outlook.com>
2020-12-26 06:48:22 -0300

    Adicionando projeto

---
+++ AmouxBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmouxBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
